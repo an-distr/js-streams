2df624fbbf7eb4f9def949c33d3ad683be081061
an <amicom.org@gmail.com>
2024-02-02 17:41:55 +0900

    Modify Cfp test function.

--- functions/json/index.js
+++ functions/json/index.js
@@ -1,11 +1,13 @@
 export function onRequest(context) {
-  const rows = context.params.rows ?? 100
+  const url = new URL(context.url)
+
+  const rows = Number(url.searchParams.get("rows") ?? "100")
   if (rows < 0 || rows > 1000) {
     return Response.error()
   }
 
   const data = []
-  for (let i = 0; i < 100; ++i) {
+  for (let i = 0; i < rows; ++i) {
     data.push({
       id: i,
       name: `data {i}`,
