672e1f0f73624fe7ee0103b218f9f07cb1ab5e86
an <amicom.org@gmail.com>
2024-02-02 21:59:18 +0900

    Add debug script for Cloudflare Pages.

--- functions/json/index.js
+++ functions/json/index.js
@@ -1,8 +1,9 @@
 export function onRequest(context) {
-  const url = new URL(context.url)
+  const request = context.request
+  const url = new URL(request.url)
 
   const rows = +(url.searchParams.get("rows") ?? "100")
-  if (rows < 0 || rows > 1000) {
+  if (rows < 0 || rows > 10000) {
     return Response.error()
   }
 
