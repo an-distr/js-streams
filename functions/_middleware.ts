931725c458bf7c3d4f5027aeaaddfcda37d591d6
an <amicom.org@gmail.com>
2024-02-04 05:55:57 +0900

    Remove cfp function.

--- functions/_middleware.ts
+++
@@ -1,8 +0,0 @@
-export const onRequest: PagesFunction = async context => {
-  const url = new URL(context.request.url)
-  const response = await context.next()
-  if (url.pathname.startsWith("/api")) {
-    response.headers.set('Access-Control-Allow-Origin', url.host)
-  }
-  return response
-}
