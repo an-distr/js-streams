d337699706c4bc006d871745ed9c1dbadef354d1
an <amicom.org@gmail.com>
2024-02-03 21:37:29 +0900

    Update html.

--- .site/site.js
+++ .site/site.js
@@ -1,5 +1,9 @@
 (() => {
 
+  if (location.host.split(".").length > 2) {
+    document.title += ` (${location.host.split(".")[0]})`
+  }
+
   const siteTheme = document.getElementById("siteTheme")
   siteTheme.value = localStorage.getItem("theme.site") ?? "auto"
   siteTheme.addEventListener("change", () => {
