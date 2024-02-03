f621f549d5eab88141abe453a805acbe46ce99fa
an <amicom.org@gmail.com>
2024-02-04 02:20:34 +0900

    Modify cfpf middleware.

--- functions/_middleware.ts
+++ functions/_middleware.ts
@@ -1,6 +1,8 @@
 export const onRequest: PagesFunction = async context => {
   const url = new URL(context.request.url)
   const response = await context.next()
-  response.headers.set('Access-Control-Allow-Origin', url.host)
+  if (url.pathname.startsWith("/api")) {
+    response.headers.set('Access-Control-Allow-Origin', url.host)
+  }
   return response
 }
