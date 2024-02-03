863a552aaf0ace5c9de08434ccff0b24d3bafd63
an <amicom.org@gmail.com>
2024-02-03 21:47:13 +0900

    Update html.

--- .site/site.js
+++ .site/site.js
@@ -2,6 +2,10 @@
 
   if (location.host.split(".").length > 2) {
     document.title += ` (${location.host.split(".")[0]})`
+    const h1 = document.getElementsByTagName("h1")[0]
+    if (h1.textContent.length === 0) {
+      h1.textContent = document.title
+    }
   }
 
   const siteTheme = document.getElementById("siteTheme")
