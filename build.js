9a888253ccb7ea9819948959bad74b70001739f6
an <amicom.org@gmail.com>
2024-02-04 17:40:23 +0900

    Add minify.

--- build.js
+++ build.js
@@ -3,11 +3,9 @@ import { glob } from "glob"
 import * as fs from "fs"
 
 // Collect targets.
-const tsFiles = (await glob("dist/**/*.ts"))
-  .filter(x => !x.includes("/functions/"))
-
-const jsFiles = tsFiles
-  .map(x => x.replace(".ts", ".js"))
+const tsFiles = (await glob("dist/**/*.ts")).filter(x => !x.includes("/functions/"))
+let jsFiles = tsFiles.map(x => x.replace(".ts", ".js"))
+jsFiles = jsFiles.concat(jsFiles.map(x => x.replace(".js", ".min.js")))
 
 // Transpiling.
 console.group("Transpiling.")
@@ -15,6 +13,25 @@ console.group("Transpiling.")
   const options = {
     entryPoints: tsFiles,
     outdir: "dist",
+    sourcemap: "linked",
+  }
+  const context = await esbuild.context(options)
+  const result = await context.rebuild()
+  console.log(result)
+  console.log(`Transpiled ${result.outputFiles?.length} file(s).`)
+}
+console.groupEnd()
+
+// Transpiling. (min)
+console.group("Transpiling. (min)")
+{
+  const options = {
+    entryPoints: tsFiles,
+    outdir: "dist",
+    minify: true,
+    outExtension: {
+      ".js": ".min.js",
+    },
     sourcemap: "linked",
   }
   const context = await esbuild.context(options)
