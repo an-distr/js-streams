b44bc55988457d12db1f896014db64ec200859f8
an <amicom.org@gmail.com>
2024-02-07 10:36:38 +0900

    Transpile.

--- dist/site/site.js
+++ dist/site/site.js
@@ -1,64 +1,59 @@
-const console = document.getElementById("console")
+const console = document.getElementById("console");
 if (console) {
-  import("/misc/DomConsole/DomConsole.min.js").then(mod => {
-    const { DomConsole } = mod
-    globalThis.console = new DomConsole(console, globalThis.console)
-
-    const dcStyle = document.createElement("link")
-    dcStyle.rel = "stylesheet"
-    dcStyle.href = "/misc/DomConsole/DomConsole.min.css"
-    document.head.appendChild(dcStyle)
-
-    const siteTheme = document.getElementById("siteTheme")
-    siteTheme.addEventListener("change", () => {
+  import("/misc/DomConsole/DomConsole.min.js").then((mod) => {
+    const { DomConsole } = mod;
+    globalThis.console = new DomConsole(console, globalThis.console);
+    const dcStyle = document.createElement("link");
+    dcStyle.rel = "stylesheet";
+    dcStyle.href = "/misc/DomConsole/DomConsole.min.css";
+    document.head.appendChild(dcStyle);
+    const siteTheme2 = document.getElementById("siteTheme");
+    siteTheme2.addEventListener("change", () => {
       const dcTheme = document.getElementById("dc-theme") ?? (() => {
-        const dcTheme = document.createElement("link")
-        dcTheme.rel = "stylesheet"
-        document.head.appendChild(dcTheme)
-        return dcTheme
-      })()
+        const dcTheme2 = document.createElement("link");
+        dcTheme2.rel = "stylesheet";
+        document.head.appendChild(dcTheme2);
+        return dcTheme2;
+      })();
       if (dcTheme) {
-        switch (siteTheme.value) {
+        switch (siteTheme2.value) {
           case "light":
-            dcTheme.href = "/misc/DomConsole/DomConsole.theme.chrome.light.min.css"
-            break
+            dcTheme.href = "/misc/DomConsole/DomConsole.theme.chrome.light.min.css";
+            break;
           case "dark":
-            dcTheme.href = "/misc/DomConsole/DomConsole.theme.chrome.dark.min.css"
-            break
+            dcTheme.href = "/misc/DomConsole/DomConsole.theme.chrome.dark.min.css";
+            break;
           default:
-            dcTheme.href = "/misc/DomConsole/DomConsole.theme.chrome.min.css"
+            dcTheme.href = "/misc/DomConsole/DomConsole.theme.chrome.min.css";
         }
       }
-    })
-    siteTheme.dispatchEvent(new Event("change"))
-  })
+    });
+    siteTheme2.dispatchEvent(new Event("change"));
+  });
+} else {
+  siteTheme.dispatchEvent(new Event("change"));
 }
-else {
-  siteTheme.dispatchEvent(new Event("change"))
-}
-
 if (location.host.startsWith("localhost") || location.host.startsWith("127.0.0.1")) {
-  document.title += " (local)"
+  document.title += " (local)";
+} else if (location.host.split(".").length > 3) {
+  document.title += ` (${location.host.split(".")[0]})`;
 }
-else if (location.host.split(".").length > 3) {
-  document.title += ` (${location.host.split(".")[0]})`
-}
-const h1 = document.getElementsByTagName("h1")[0]
+const h1 = document.getElementsByTagName("h1")[0];
 if (h1.textContent.length === 0) {
-  h1.textContent = document.title
+  h1.textContent = document.title;
 }
-
-const toTop = document.createElement("a")
-toTop.style.position = "fixed"
-toTop.style.bottom = "0"
-toTop.style.right = "0"
-toTop.style.marginBottom = "1rem"
-toTop.style.marginRight = "1rem"
-toTop.style.cursor = "pointer"
-toTop.href = "#"
-toTop.textContent = "Top"
-toTop.onclick = e => {
-  e.preventDefault()
-  window.scroll({ top: 0 })
-}
-document.body.append(toTop)
+const toTop = document.createElement("a");
+toTop.style.position = "fixed";
+toTop.style.bottom = "0";
+toTop.style.right = "0";
+toTop.style.marginBottom = "1rem";
+toTop.style.marginRight = "1rem";
+toTop.style.cursor = "pointer";
+toTop.href = "#";
+toTop.textContent = "Top";
+toTop.onclick = (e) => {
+  e.preventDefault();
+  window.scroll({ top: 0 });
+};
+document.body.append(toTop);
+//# sourceMappingURL=site.js.map
