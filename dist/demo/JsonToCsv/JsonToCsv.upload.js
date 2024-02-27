afaba92130556aa2482a34491050ed4364cfa17f
an <amicom.org@gmail.com>
2024-02-27 14:29:34 +0900

    Update JsonToCsv demo.

--- dist/demo/JsonToCsv/JsonToCsv.upload.js
+++ dist/demo/JsonToCsv/JsonToCsv.upload.js
@@ -1,10 +1,23 @@
 import * as streams from "/web.js";
 const chkDirect = document.getElementById("chkDirect");
+const rdoInputFormatJSON = document.getElementById("rdoInputFormatJSON");
 const rdoInputFormatJSONL = document.getElementById("rdoInputFormatJSONL");
 const rdoInputFormatJSONC = document.getElementById("rdoInputFormatJSONC");
 const rdoOutputFormatCSV = document.getElementById("rdoOutputFormatCSV");
 const txtFile = document.getElementById("txtFile");
 const linkHolder = txtFile.parentElement.lastElementChild;
+const onChangeInputFromat = () => {
+  if (rdoInputFormatJSON.checked) {
+    txtFile.accept = ".json";
+  } else if (rdoInputFormatJSONL.checked) {
+    txtFile.accept = ".jsonl";
+  } else if (rdoInputFormatJSONC.checked) {
+    txtFile.accept = ".jsonc";
+  }
+};
+rdoInputFormatJSON.addEventListener("change", onChangeInputFromat);
+rdoInputFormatJSONL.addEventListener("change", onChangeInputFromat);
+rdoInputFormatJSONC.addEventListener("change", onChangeInputFromat);
 txtFile.onchange = () => {
   if (!txtFile.files) {
     return;
--- dist/demo/JsonToCsv/JsonToCsv.upload.ts
+++ dist/demo/JsonToCsv/JsonToCsv.upload.ts
@@ -2,11 +2,27 @@
 import * as streams from "/web.ts"
 
 const chkDirect = document.getElementById("chkDirect") as HTMLInputElement
+const rdoInputFormatJSON = document.getElementById("rdoInputFormatJSON") as HTMLInputElement
 const rdoInputFormatJSONL = document.getElementById("rdoInputFormatJSONL") as HTMLInputElement
 const rdoInputFormatJSONC = document.getElementById("rdoInputFormatJSONC") as HTMLInputElement
 const rdoOutputFormatCSV = document.getElementById("rdoOutputFormatCSV") as HTMLInputElement
 const txtFile = document.getElementById("txtFile") as HTMLInputElement
 const linkHolder = txtFile.parentElement!.lastElementChild as HTMLDivElement
+
+const onChangeInputFromat = () => {
+  if (rdoInputFormatJSON.checked) {
+    txtFile.accept = ".json"
+  }
+  else if (rdoInputFormatJSONL.checked) {
+    txtFile.accept = ".jsonl"
+  }
+  else if (rdoInputFormatJSONC.checked) {
+    txtFile.accept = ".jsonc"
+  }
+}
+rdoInputFormatJSON.addEventListener("change", onChangeInputFromat)
+rdoInputFormatJSONL.addEventListener("change", onChangeInputFromat)
+rdoInputFormatJSONC.addEventListener("change", onChangeInputFromat)
 
 txtFile.onchange = () => {
   if (!txtFile.files) {
